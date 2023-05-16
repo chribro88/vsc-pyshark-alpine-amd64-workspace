9db322db8b6ee9d2863d65cc37a26b77c2fd7324
chribro88 <chrisbrown@live.com.au>
2023-05-16 19:49:55 +1000

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,25 +2,25 @@
 
 CONDA_DIR=/workspace/.conda
 TMP_FILE=/tmp/conda-tmp/environment.yml
-FILE=$TMP_FILE/environment.yml
+FILE=$CONDA_DIR/environment.yml
 
 if [ ! -d "$CONDA_DIR" ]; then
 mkdir "$CONDA_DIR"
 fi
 
 if [ ! -f "$FILE" ]; then
-    cp "$TMP_FILE" "$FILE"
+    cp "$TMP_FILE" "$CONDA_DIR"
 fi
 
 # Create Virtual Environment
 if [ -f "$FILE" ]; then
-    env_name=$(cat /workspace/.conda/environment.yml | grep "name: " | awk -F": " '{print $2}')
+    env_name=$(cat $FILE | grep "name: " | awk -F": " '{print $2}')
     echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc
     echo "conda activate" >> ~/.bashrc
     echo "conda activate $env_name" >> ~/.bashrc
     echo "clear" >> ~/.bashrc
     # attempt fix weird terminal output by setting COLUMNS (still doesn't work)
-    conda env create --file /workspace/.conda/environment.yml --prefix=/workspace/.conda/envs/$env_name --force
+    conda env create --file $FILE --prefix=$CONDA_DIR/envs/$env_name --force
     # rm -f /tmp/conda-tmp/environment.yml
 fi
 
