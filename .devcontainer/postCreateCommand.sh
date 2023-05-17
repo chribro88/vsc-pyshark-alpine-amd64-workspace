f969a014d35c850cb1255183fa4e73c9d7114e94
chribro88 <chrisbrown@live.com.au>
2023-05-17 12:49:03 +1000

    update conda script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -12,15 +12,18 @@ if [ ! -f "$FILE" ]; then
     cp "$TMP_FILE" "$CONDA_DIR"
 fi
 
+export COLUMNS=55
+
 # Create Virtual Environment
 if [ -f "$FILE" ]; then
     env_name=$(cat $FILE | grep "name: " | awk -F": " '{print $2}')
     echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc
     echo "conda activate" >> ~/.bashrc
-    echo "conda activate $CONDA_DIR/envs/$env_name" >> ~/.bashrc
+    echo "conda activate $env_name" >> ~/.bashrc
     echo "clear" >> ~/.bashrc
     # attempt fix weird terminal output by setting COLUMNS (still doesn't work)
-    COLUMNS=55 conda env create --file $FILE --prefix=$CONDA_DIR/envs/$env_name --force
+    conda config --append envs_dirs /workspace/.conda/envs/
+    conda env create --file $FILE --prefix=$CONDA_DIR/envs/$env_name --force
     # rm -f /tmp/conda-tmp/environment.yml
 fi
 
